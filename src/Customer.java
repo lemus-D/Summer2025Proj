cc6d767fbe474123cf7b28401992a273a0bcfd76
Darwin L <lemus@unc.edu>
2025-07-30 10:20:17 -0400

    started the customer class

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,26 @@
 public class Customer {
+    private String name;
+    private route route;
+    private String Address;
+    private int phoneNumber;
+
+    public Customer(String name, route route, String Address, int phoneNumber) {
+        this.name = name;
+        this.route = route;
+        this.Address = Address;
+        this.phoneNumber = phoneNumber;
+    }
+
+
+
+
+
+
+
+
+
+    public enum route {
+        MON, TUE, WED, THU, FRI, SAT
+    }
+
 }
